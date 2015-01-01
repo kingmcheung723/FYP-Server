d7ee61536eb614ecdcb592cea03e7253f87337c2
King Cheung <kingmcheung723@gmail.com>
2015-01-01 23:14:54 +0800

    Read the goods table

--- FYP-Server/src/DataGrabber.java
+++ FYP-Server/src/DataGrabber.java
@@ -6,27 +6,41 @@ import java.io.UnsupportedEncodingException;
 import java.net.URL;
 
 public class DataGrabber {
+	
+	private final static String fileExtension = ".html";
 
 	public String getHtmlSource(String url) throws Exception {
+		final String startStr = "<table border=1 cellspacing=0 cellpadding=2 width=945>";
+		final String endStr = "</table>";
 		String htmlSource = new String();
 		URL oracle = new URL(url);
-		BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+		BufferedReader in = new BufferedReader(new InputStreamReader(
+				oracle.openStream()));
 
 		String inputLine;
+		boolean isStarted = false;
 		while ((inputLine = in.readLine()) != null) {
-			htmlSource += inputLine + "\n";
+			// Read the Goods table
+			if (inputLine.equalsIgnoreCase(startStr) || isStarted) {
+				isStarted = true;
+				htmlSource += inputLine+"\n";
+				if (inputLine.equalsIgnoreCase(endStr)) {
+					break;
+				}
+			}
 		}
 		in.close();
+		
 
 		System.out.println(htmlSource);
 		writeToFile(htmlSource);
 		return htmlSource;
 	}
 
-	public void writeToFile(String stringToWrite) throws FileNotFoundException, UnsupportedEncodingException {
-		PrintWriter writer = new PrintWriter("the-file-name.html", "UTF-8");
+	private void writeToFile(String stringToWrite) throws FileNotFoundException,
+			UnsupportedEncodingException {
+		PrintWriter writer = new PrintWriter("the-file-name"+fileExtension, "UTF-8");
 		writer.println(stringToWrite);
 		writer.close();
 	}
-
 }
