5fdb8cadf00b146c8463a261218a4ce1911eda16
King Cheung <kingmcheung723@gmail.com>
2015-01-02 00:18:06 +0800

    Jsoup added Extract elements from html implemented

--- FYP-Server/src/main.java
+++ FYP-Server/src/main.java
@@ -5,7 +5,8 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		DataGrabber dg = new DataGrabber();
-		dg.getHtmlSource(consumerURLEN);
+//		dg.getHtmlSource(consumerURLEN);
+		dg.extractHtmlElement(consumerURLZH);
 		
 	}
 }
