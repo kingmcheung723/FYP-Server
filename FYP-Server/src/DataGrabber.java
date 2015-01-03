b8050ff271a91c1cd8eedf2650a54c502035f42f
King Cheung <kingmcheung723@gmail.com>
2015-01-03 22:19:07 +0800

    getHtmlSource removed

--- FYP-Server/src/DataGrabber.java
+++ FYP-Server/src/DataGrabber.java
@@ -19,49 +19,14 @@ public class DataGrabber {
 
 	private final static String fileExtension = ".html";
 	
-	private List<String> goods = new ArrayList<>();
-
-	public String getHtmlSource(String url) throws Exception {
-		final String startStr = "<table border=1 cellspacing=0 cellpadding=2 width=945>";
-		final String endStr = "</table>";
-		final String startRowTag = "<tr";
-		final String engRowTag = "</tr>";
-		final String startColTag = "<td";
-		final String engColTag = "</td>";
-
-		String htmlSource = new String();
-		URL oracle = new URL(url);
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-				oracle.openStream()));
-
-		String inputLine;
-		boolean isStarted = false;
-		while ((inputLine = in.readLine()) != null) {
-			// Read the Goods table
-			if (inputLine.equalsIgnoreCase(startStr) || isStarted) {
-				isStarted = true;
-				htmlSource += inputLine + "\n";
-
-				// If the line reach </table> means the table ended
-				if (inputLine.equalsIgnoreCase(endStr)) {
-					// Break the loop
-					break;
-				}
-			}
-		}
-		in.close();
-
-		System.out.println(htmlSource);
-		writeToFile(htmlSource);
-		return htmlSource;
-	}
-
+	private List<Goods> goods = new ArrayList<>();
+	
 	public void extractHtmlElement(String urlStr) throws IOException {
 		URL url = new URL(urlStr);
 		// Connection timeout in millisecond
 		int timeOut = 30000;
-		Document doc = Jsoup.parse(url, timeOut);
-		Element table = doc.select("table").get(9); // select the first table.
+		Document htmlSource = Jsoup.parse(url, timeOut);
+		Element table = htmlSource.select("table").get(9); // select the first table.
 		Elements rows = table.select("tr");
 		String record = "";
 		for (int i = 1; i < rows.size(); i++) { // first row is the col names so skip it.
--- FYP-Server/src/Main.java
+++ FYP-Server/src/Main.java
@@ -14,7 +14,6 @@ public class Main {
 	 */
 	public static void main(String[] args) throws IOException {
 		DataGrabber dg = new DataGrabber();
-//		dg.getHtmlSource(consumerURLEN);
 		dg.extractHtmlElement(consumerURLZH);
 
 	}
