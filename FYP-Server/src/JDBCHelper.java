ba7da8226c98e60101790c18d052f90596483fce
King Cheung <kingmcheung723@gmail.com>
2015-01-05 23:59:01 +0800

    Override JDBCHelper execute function

--- FYP-Server/src/JDBCHelper.java
+++ FYP-Server/src/JDBCHelper.java
@@ -3,54 +3,85 @@
  */
 package src;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.DataSource;
 
+import jdbchelper.JdbcException;
 import jdbchelper.JdbcHelper;
+import jdbchelper.JdbcUtil;
 import jdbchelper.SimpleDataSource;
 
 /**
  * @author CKM
  * 
  */
-public class JDBCHelper {
+public class JDBCHelper extends JdbcHelper {
 
 	private static JDBCHelper instance = null;
-	private JdbcHelper jdbcHelper;
 
 	/**
 	 * Constructor
 	 */
-	public JDBCHelper() {
-		super();
-
-		DataSource dataSource = new SimpleDataSource(
-				"com.mysql.jdbc.Driver",
-				"jdbc:mysql://localhost:3306/fyp_db?useUnicode=yes&characterEncoding=UTF-8",
-				"root", null);
-		jdbcHelper = new JdbcHelper(dataSource);
+	public JDBCHelper(DataSource dataSource) {
+		super(dataSource);
+		instance = this;
 	}
 
 	public static JDBCHelper getInstance() {
 		if (instance == null) {
-			instance = new JDBCHelper();
+			DataSource dataSource = new SimpleDataSource(
+					"com.mysql.jdbc.Driver",
+					"jdbc:mysql://localhost:3306/fyp_db?useUnicode=yes&characterEncoding=UTF-8",
+					"root", null);
+			instance = new JDBCHelper(dataSource);
 		}
 		return instance;
 	}
+	
+	   /**
+	    * <p>This method may be used in executing statements like insert/update/delete on the server.</p>
+	    *
+	    * <p>Example:</p>
+	    * <pre>
+	    * int updatedUsers = jdbc.execute("update users set active = 0 where register_date < ?", "2009-01-01");
+	    * </pre>
+	    * <p>If something goes wrong with the execution of the statement, a JdbcException may be thrown</p>
+	    *
+	    * @param sql The sql statement to be executed on the server
+	    * @param params Optional parameteres that will be used as prepared statement parameters
+	    * @return Returns the number of affected rows
+	    */
+	   public int execute(String sql, Object... params) {
+	      Connection con = null;
+	      Statement stmt = null;
+
+	      try {
+	         con = getConnection();
+
+	         if (params.length == 0) {
+	            stmt = con.createStatement();
+	            return stmt.executeUpdate(sql);
+	         } else {
+	            stmt = fillStatement(con.prepareStatement(sql), params);
+	            return ((PreparedStatement) stmt).executeUpdate();
+	         }
+	      } catch (SQLException e) {
+	         throw new JdbcException("Error executing query:\n" + sql + "\n\nError: " + e.getMessage(), e);
+	      } finally {
+	         JdbcUtil.close(stmt);
+	         freeConnection(con);
+	      }
+	      
+	   }
 
 	/**
 	 * @return the jdbcHelper
 	 */
-	public JdbcHelper getJdbcHelper() {
-		return jdbcHelper;
-	}
-
-	/**
-	 * @param jdbcHelper
-	 *            the jdbcHelper to set
-	 */
-	public void setJdbcHelper(JdbcHelper jdbcHelper) {
-		this.jdbcHelper = jdbcHelper;
+	public JDBCHelper getJDBCHelper() {
+		return instance;
 	}
 }
